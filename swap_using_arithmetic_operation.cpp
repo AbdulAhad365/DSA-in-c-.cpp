5d1cb13e6172dc3eca39c2445c1d3fc6cd95e59b
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2023-07-01 01:04:13 +0500

    Create swap_using_arithmetic_operation.cpp

---
+++ swap_using_arithmetic_operation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    int a=3;
+    int b=4;
+    a=a+b;// 7
+    b=a-b;//3
+    a=a-b;
+    cout<<"a: "<<a<<" b: "<<b;
+    return 0;
+}
